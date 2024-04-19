7af72aeb78249622d2549f57fca2aacd5826da8d
DongHoon Kim <donghoon5793@gmail.com>
2024-04-19 01:06:50 -0600

    pip install error fix test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="casepy",
-    version="0.0.3",
+    version="0.0.3a1",
     description="A Python package for generating cases in a list.",
     url="https://github.com/DongHoon5793/casepy",
     long_description=open("README.md", "r").read(),
@@ -11,7 +11,7 @@ setup(
     author="DongHoon Kim",
     author_email="donghoon5793@gmail.com",
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">2.7.0",
+    packages=find_packages(include=["casepy", "casepy.*"]),
+    # python_requires=">=2.7.0",
     install_requires=[],
 )
