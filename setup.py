cf4c003a5770cf58385d7ee407a734206b41a9a0
DongHoon Kim <donghoon5793@gmail.com>
2024-04-19 00:27:07 -0600

    v0.0.1 -> v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="casepy",
-    version="0.0.1",
+    version="0.0.2",
     description="A Python package for generating cases in a list.",
     url="https://github.com/DongHoon5793/casepy",
     long_description=open("README.md", "r").read(),
