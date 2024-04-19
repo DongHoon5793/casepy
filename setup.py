11426bb534a72d45a680152ef6a16b28a09b000e
DongHoon Kim <donghoon5793@gmail.com>
2024-04-18 23:59:54 -0600

    minor modify

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="casepy",
     version="0.0.1",
     description="A Python package for generating cases in a list.",
-    url="https://github.com/donghoon5793/casepy",
+    url="https://github.com/DongHoon5793/casepy",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     license="MIT",
