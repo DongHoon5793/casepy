09f71cec6ab6336ff81212e7bc2d531280e656b5
DongHoon Kim <donghoon5793@gmail.com>
2024-06-26 22:27:12 +0900

    add all_combinations_unique_elements

--- src/casepy/simple_methods.py
+++ src/casepy/simple_methods.py
@@ -20,6 +20,46 @@ def combination_total(in_number_of_elements: int, in_number_of_select: int) -> i
     )
 
 
+def all_combinations_unique_elements(
+    in_number_of_selection: int, in_elements: list
+) -> list:
+    """
+    Return all combinations of the given number of selections from a list that has unique elements only.
+
+    Args:
+        in_number_of_selection (int): The number of selections.
+        in_elements (list): The list of elements.
+
+    Returns:
+        list: All combinations of the given number.
+    """
+    result = []
+
+    # Check if the number of selection is valid.
+    if len(in_elements) < in_number_of_selection or in_number_of_selection < 1:
+        return []
+    if len(in_elements) == in_number_of_selection:
+        return [in_elements]
+
+    # If the number of selection is 1, return the list of elements.
+    if in_number_of_selection == 1:
+        for i in in_elements:
+            result.append([i])
+        return result
+
+    # If the number of selection is larger than 1, do the following.
+    # Cut the first element
+    first, rest = in_elements[0], in_elements[1:]
+
+    result = list(
+        map(
+            lambda x: [first] + x,
+            all_combinations_unique_elements(in_number_of_selection - 1, rest),
+        )
+    ) + all_combinations_unique_elements(in_number_of_selection, rest)
+    return result
+
+
 def permutation_total(in_number_of_elements: int, in_number_of_select: int) -> int:
     """
     Return the number of all possible permutation cases (tranditional method).
